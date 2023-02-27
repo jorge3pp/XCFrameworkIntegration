f20673c29758ae8d517596f4c8e606e4d4f69136
Jorge Poveda <jpoveda@facephi.com>
2023-02-27 13:35:54 +0100

    Created PackageDepA.xcframework

--- PackageDepA/Package.swift
+++ PackageDepA/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "PackageDepA",
+            name: "PackageDepA",  
             targets: ["PackageDepA"]),
     ],
     dependencies: [
