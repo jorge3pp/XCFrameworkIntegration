197fc2d64892daf2aea10e4a93cb55bb3777d261
Jorge Poveda <jpoveda@facephi.com>
2023-02-27 13:58:16 +0100

    Added dependency to PackageDepA in IntegratorApp

--- IntegratorApp/ViewController.swift
+++ IntegratorApp/ViewController.swift
@@ -6,14 +6,13 @@
 //
 
 import UIKit
+import PackageDepA
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        PackageDepA().version
     }
-
-
 }
-
